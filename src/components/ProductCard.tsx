6469a5a7d530c119291ffd366f03cad5275f2242
sazzad.jafor@technonext.com <sazzad.jafor@technonext.com>
2026-04-01 23:36:57 +0600

    ref: update product card design according to figma

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -2,26 +2,26 @@ import type { Product } from "../types/product";
 
 function ProductCard({ product }: {  product: Product}) {
   return (
-    <div key={product.id} className="bg-white rounded-2xl border border-[var(--border)] overflow-hidden hover:shadow-md transition">
+    <div key={product.id} className="rounded-md overflow-hidden hover:shadow-md transition">
 
       {/* Image */}
-      <div className="bg-gray-100 flex items-center justify-center h-48">
+      <div className="bg-gray-100 flex items-center justify-center h-[210px]">
         <img
           src={product.imageUrl}
           alt="Product"
-          className="object-cover h-full"
+          className="object-cover h-full rounded-tl-md rounded-tr-md rounded-bl-[4px] rounded-br-[4px]"
         />
       </div>
 
       {/* Content */}
-      <div className="p-4">
-        <p className="text-sm text-gray-500 mb-1">{product.category}</p>
+      <div className="p-2">
+        <p className="text-sm mb-[2px] text-dark700">{product.category}</p>
 
-        <h3 className="text-sm font-semibold text-gray-800 line-clamp-2 mb-2">
+        <h3 className="text-base font-medium line-clamp-2 mb-2 text-dark900">
           {product.name}
         </h3>
 
-        <p className="text-lg font-bold text-[var(--primary)]">
+        <p className="text-xl font-medium text-primary700">
           ৳ {product.price}
         </p>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark700: '#5A6573',
+        dark900: '#1A2B3D',
+        primary700: '#1882FF',
+      },
+    },
   },
   plugins: [],
 }
